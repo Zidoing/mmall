e63812709f529b482cca22854ac401f5bae27f78
zhoulei <403907871@qq.com>
2021-05-27 17:55:16 +0800

    - 完成主体组件

---
+++ src/pages/destination/index.js
@@ -0,0 +1,11 @@
+import 'styles/reset.css'
+import 'styles/base.css'
+import 'styles/layout.css'
+import './destination.css'
+
+import 'components/tabbar'
+import 'components/backtop'
+
+import './components/header'
+import './components/main'
+
