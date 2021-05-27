b8072a6de6ce4aca2aa8abd6b35d1a506434ce5d
zhoulei <403907871@qq.com>
2021-05-27 14:11:05 +0800

    - 代码优化

--- src/components/backtop/index.js
+++ src/components/backtop/index.js
@@ -1,70 +1,66 @@
-import './backtop.css'
-import 'icons/iconfont.css'
+import './backtop.css';
+import 'icons/iconfont.css';
 
-const CHANGED_CLASS_NAME = "backtop-hidden";
-const INIT_STATE = "init"
-const CHANGED_STATE = "changed";
+import Scroll from 'utils/scroll';
+
+const CHANGED_CLASS_NAME = 'backtop-hidden';
 
 class Backtop {
     constructor(el, critical_point, scrollContainer, eventEl = scrollContainer) {
         this.el = el;
         this.critical_point = critical_point;
+
         // 滚动条所在的容器
         this.scrollContainer = scrollContainer;
+
         // 监听滚动事件的元素
         this.eventEl = eventEl;
 
-        this.setState(INIT_STATE);
+        new Scroll(
+            {
+                critical_point,
+                change: () => {
+                    this.show();
+                },
+                reset: () => {
+                    this.hide();
+                }
+            },
+            scrollContainer,
+            eventEl
+        );
+
         this.bindEvent();
     }
 
-    setState(state) {
-        this.state = state;
-    }
-
+    // 绑定事件
     bindEvent() {
-        this.eventEl.addEventListener("scroll", () => {
-            if (this.needChange()) {
-                this.setState(CHANGED_STATE);
-                console.log("changed")
-                this.change()
-
-            } else if (this.needReset()) {
-                console.log("reset")
-                this.setState(INIT_STATE)
-                this.reset()
-            }
-        }, false);
-
-        this.eventEl.addEventListener("click", () => {
-            this.scrollTo()
-        }, false)
+        this.el.addEventListener(
+            'click',
+            () => {
+                this.scrollTo();
+            },
+            false
+        );
     }
 
     scrollTo(top = 0, left = 0) {
         this.scrollContainer.scrollTo({
-            top, left, behavior: 'smooth'
-        })
-
+            top,
+            left,
+            behavior: 'smooth'
+        });
     }
 
-    needChange() {
-        return this.state !== CHANGED_STATE && this.scrollContainer.scrollTop > this.critical_point;
-    }
-
-    change() {
-        this.el.classList.remove(CHANGED_CLASS_NAME)
-    }
-
-    needReset() {
-        return this.state !== INIT_STATE && this.scrollContainer.scrollTop <= this.critical_point
-    }
-
-    reset() {
+    //   隐藏
+    hide() {
         this.el.classList.add(CHANGED_CLASS_NAME);
     }
 
-
+    // 显示
+    show() {
+        this.el.classList.remove(CHANGED_CLASS_NAME);
+    }
 }
 
 export default Backtop;
---
+++ src/utils/scroll/defaults.js
@@ -0,0 +1,9 @@
+const DEFAULTS = {
+    critical_point: 0,
+    change() {
+    },
+    reset() {
+    }
+};
+
+export default DEFAULTS;
