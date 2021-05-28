9ff75f4ced9754e085f761da31088cbec0237d1f
zhoulei <403907871@qq.com>
2021-05-28 09:42:49 +0800

    - 完成内容组件

--- src/pages/destination/components/main/index.js
+++ src/pages/destination/components/main/index.js
@@ -1,4 +1,20 @@
 import './main.css'
-import '../tab'
+import Tab from "../tab";
+import Content from "../content";
 import '../content'
 import 'components/loading'
+
+
+const tabEl = document.querySelector(".tab")
+const tab = new Tab(tabEl);
+const content = new Content(document.getElementById("destination-content"))
+// tab.to(2).then(data => content.set(data))
+
+tabEl.addEventListener("click", function (e) {
+    const item = e.target;
+    content.setLoading()
+    const index = item.dataset.id - 1
+    tab.to(index).then(data => content.set(data))
+});
+
+tabEl.children[0].click()
--- src/pages/destination/components/tab/index.js
+++ src/pages/destination/components/tab/index.js
@@ -1,1 +1,28 @@
 import './tab.css'
+
+
+import {getData} from "../../../../api/getData";
+
+
+class Tab {
+    constructor(el) {
+        this.itemEls = el.querySelectorAll('.tab-item')
+    }
+
+    setActiveItem(index) {
+        this.itemEls.forEach(item => {
+            item.classList.remove("tab-item-active")
+        })
+        this.itemEls[index].classList.add("tab-item-active")
+    }
+
+
+    to(index) {
+        this.setActiveItem(index);
+        return getData(`${URL}/${this.itemEls[index].dataset.id}`)
+    }
+}
+
+const URL = 'https://www.imooc.com/api/mall-wepApp/destination/content'
+
+export default Tab;
